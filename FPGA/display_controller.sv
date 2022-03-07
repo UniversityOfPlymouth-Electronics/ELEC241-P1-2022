3039e8a3727507cb161d501213dba988964aabb2
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2022-03-07 15:50:44 +0000

    Fixed issue with pin mapping

--- FPGA/display_controller.sv
+++ FPGA/display_controller.sv
@@ -7,30 +7,11 @@ module display_controller (
    input logic write,
    input logic clk);
    
-   // ROM for testing the LCD
-	reg [10:0] rom[2:0] = '{
-	
-      //
-		{3'b000,8'b00000000}, 
-		{3'b000,8'b00000000}, 
-		{3'b000,8'b00000000}, 
-		{3'b000,8'b00000000}, 
-		{3'b000,8'b00000000}, 
-		{3'b000,8'b00000000}, 
-		{3'b000,8'b00000000}, 
-		{3'b000,8'b00000000}
-      
-		}; /* synthesis romstyle = "M9K" */
- 
-   logic [2:0] addr = '0; 
+
  
 always_ff @(posedge clk) begin
 
-   
-   data <= rom[addr][7:0];
-   rs   <= rom[addr][8];
-   rw   <= rom[addr][9];
-   e    <= rom[addr][10];
+
 
 end
    
